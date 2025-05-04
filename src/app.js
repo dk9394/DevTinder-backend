2b987adb2169327456d059c284fde10b4b12cc04
dk9394 <k.deepak9394@gmail.com>
2025-05-05 04:52:52 +0530

    Routes pattern - query params and dynamic routes

--- src/app.js
+++ src/app.js
@@ -2,33 +2,62 @@ const express = require('express');
 
 const app = express();
 
-// Make API enpoint
-app.use('/test', (req, res) => {
-	res.send('Test');
-});
+/**
+ * Routes pattern
+ *
+ * The ? makes anything after it optional
+ * The + allows repeating anything which is followed by it
+ * The * allows anything random in between in place of it
+ * The () makes optional which is in between it
+ * RegEx is also allowed instead of plain string values as route
+ */
 
-// This will handle GET method for /user
+// Here, /ab, /abc both will work
+// app.get('/ab?c', (req, res) => {
+// 	res.send({ name: 'Deepak' });
+// });
+
+// Here, repeating b is allowed, /abbbbbbbc, /abc both will work
+// app.get('/ab+c', (req, res) => {
+// 	res.send({ name: 'Deepak' });
+// });
+
+// Here, /abcd, /abXYZcd both will work
+// app.get('/ab*cd', (req, res) => {
+// 	res.send({ name: 'Deepak' });
+// });
+
+// Here, /ad, /abcd both will work, because bc is optional in the route
+// app.get('/a(bc)d', (req, res) => {
+// 	res.send({ name: 'Deepak' });
+// });
+
+// Here, bc is optional as well as repeatable, /ad, /abcd, /abcbcbcd all will work
+// app.get('/a(bc)+d', (req, res) => {
+// 	res.send({ name: 'Deepak' });
+// });
+
+/**
+ * Route query params and dynamic routes
+ */
+
+// The query params are passed using, ?key=val&key=val pattern. Here & is used for more than 1 query param
+// Example: /user?userId=101&pwd=test
 app.get('/user', (req, res) => {
+	// Accessing the query params for the specific route
+	console.log(req.query);
 	res.send({ name: 'Deepak' });
 });
 
-// This will handle POST method for /user
-app.post('/user', (req, res) => {
-	res.send('Data saved!');
+// The dynamic routes are used like this, /user/101/Deepak/xyz pattern.
+// Here every dynamic route value will be captured by the defined key in the route
+app.get('/user/:userId/:name/:pwd', (req, res) => {
+	// Accessing dynamic route values for the specific route
+	console.log(req.params);
+	console.log(req.query);
+	res.send({ name: 'Deepak' });
 });
 
-// This will handle DELETE method for /user
-app.delete('/user', (req, res) => {
-	res.send('user deleted');
-});
-
-// The use method handle all matching routes regardsless of the HTTP method type (GET, POST, PUT, PATCH, DELETE)
-// Order of endpoint routes is important - putting empty / in the last makes the pervious routes working
-app.use('/', (req, res) => {
-	res.send('Hello from the server!');
-});
-
-// Make server listen to the mentioned port - 3000
 app.listen(3000, () => {
 	console.log('Server is successfully listening on port 3000');
 });
