a5da3cf59089eb78fd3c537c72b94e8feea69e54
dk9394 <k.deepak9394@gmail.com>
2025-05-06 23:05:07 +0530

    user api, schema and validation added

--- src/app.js
+++ src/app.js
@@ -5,21 +5,84 @@ const { UserModel } = require('./models/user');
 
 const app = express();
 
+app.use(express.json());
+
 app.post('/signup', async (req, res) => {
-	const user = new UserModel({
-		firstName: 'Deepak',
-		lastName: 'Kumar',
-		emailId: 'k.deepak9394@gmail.com',
-		password: 'devTinder@9394',
-		age: 35,
-		gender: 'male',
-	});
+	const user = new UserModel(req.body);
 
 	try {
 		await user.save();
 		res.send('User added successfully!');
 	} catch (err) {
-		res.status(400).send('Error saving the user: ', err.message);
+		res.status(400).send(err.message);
+	}
+});
+
+app.patch('/user/:userId', async (req, res) => {
+	const user = req.body;
+	const userId = req.params.userId;
+	const ALLOWED_UPDATES = ['firstName', 'lastName', 'age', 'gender', 'profileIconUrl', 'gallery', 'about', 'skills'];
+	const isUpdateAllowed = Object.keys(user).every((key) => ALLOWED_UPDATES.includes(key));
+
+	try {
+		if (!isUpdateAllowed) {
+			throw new Error('Wrong data is not allowed to update!');
+		}
+		await UserModel.findByIdAndUpdate(userId, user, { runValidators: true });
+		res.send('User updated successfully!');
+	} catch (err) {
+		res.status(400).send(err.message);
+	}
+});
+
+app.get('/getUser/:userId', async (req, res) => {
+	const id = req.params.userId;
+
+	try {
+		const user = await UserModel.findOne({ _id: id });
+		if (user) {
+			res.send(user);
+		} else {
+			res.status(404).send('User not found with matching emailId: ' + userEmailId);
+		}
+	} catch (err) {
+		res.status(400).send(err.message);
+	}
+});
+
+app.get('/getUsers', async (req, res) => {
+	const userName = req.body.firstName;
+
+	try {
+		const users = await UserModel.find({ firstName: userName });
+		if (users.length) {
+			res.send(users);
+		} else {
+			res.status(404).send('No user found matching: ' + userName);
+		}
+	} catch (err) {
+		res.status(400).send(err.message);
+	}
+});
+
+app.get('/getAllUsers', async (req, res) => {
+	try {
+		const users = await UserModel.find({});
+		res.send(users);
+	} catch (err) {
+		res.status(400).send(err.message);
+	}
+});
+
+app.delete('/user/:userId', async (req, res) => {
+	const id = req.params.userId;
+
+	try {
+		// const user = await UserModel.findByIdAndDelete({ _id: id });
+		const user = await UserModel.findByIdAndDelete(id);
+		res.send('User deleted successfully!');
+	} catch (err) {
+		res.status(400).send(err.message);
 	}
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,91 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
-const userSchema = mongoose.Schema({
-	firstName: {
-		type: String,
+const userSchema = mongoose.Schema(
+	{
+		firstName: {
+			type: String,
+			required: true,
+			trim: true,
+			minLength: 3,
+			maxLength: 50,
+		},
+		lastName: {
+			type: String,
+			trim: true,
+			minLength: 3,
+			maxLength: 50,
+		},
+		emailId: {
+			type: String,
+			required: true,
+			unique: true,
+			lowercase: true,
+			trim: true,
+			validate(value) {
+				if (!validator.isEmail(value)) {
+					throw new Error('Invalid email address: ' + value);
+				}
+			},
+		},
+		password: {
+			type: String,
+			required: true,
+			validate(value) {
+				if (!validator.isStrongPassword(value)) {
+					throw new Error('Not a strong password: ' + value);
+				}
+			},
+		},
+		age: {
+			type: Number,
+			min: 18,
+		},
+		gender: {
+			type: String,
+			validate(value) {
+				if (!['male', 'female', 'others'].includes(value)) {
+					throw new Error('Gender is not valid!');
+				}
+			},
+		},
+		profileIconUrl: {
+			type: String,
+			default: 'https://www.pngfind.com/pngs/m/665-6650067_business-man-signo-de-estudiantes-hd-png-download.png',
+			validate(value) {
+				if (!validator.isURL(value)) {
+					throw new Error('Invalid image URL: ' + value);
+				}
+			},
+		},
+		gallery: {
+			type: [String],
+			default: [],
+			validate(value) {
+				if (!value.every((val) => validator.isUrl(val))) {
+					throw new Error('One or more invalid gallery image URL');
+				}
+			},
+		},
+		about: {
+			type: String,
+			trim: true,
+			minLength: 6,
+			maxLength: 100,
+		},
+		skills: {
+			type: [String],
+			validate(value) {
+				if (value.length > 10) {
+					throw new Error('More than 10 skills are not allowed!');
+				}
+			},
+		},
 	},
-	lastName: {
-		type: String,
-	},
-	emailId: {
-		type: String,
-	},
-	password: {
-		type: String,
-	},
-	age: {
-		type: Number,
-	},
-	gender: {
-		type: String,
-	},
-});
+	{
+		timestamps: true,
+	}
+);
 
 const UserModel = mongoose.model('User', userSchema);
 
